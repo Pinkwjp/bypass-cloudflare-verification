b58f2ccde00572ac6565251e7f4bccbcd45d9595
Jianping <pinkwjp@gmail.com>
2025-02-17 14:19:13 -0500

    clean up

--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,3 @@
-# python -m app
-
-# NOTE: when encounter 'Xlib.error.DisplayConnectionError'
-# in Linux terminal run: 'xhost +' 
-# response: 'access control disabled, clients can connect from any host'
-
-# NOTE: not able to handld cloudflare with builtin seleniumbase methods
-
-# NOTE: not able to locate cloudflare iframe with driver.find_element
-
-# NOTE: on Fedora, need Python 3.10 to be compatible with pyautogui and its dependencies
-
-# NOTE: seems like pyautogui.locateCenterOnScreen prefer simple image with simple lines
-
-# NOTE: As of pyautogui version 0.9.41, if the locate functions can’t find the provided image, 
-# they’ll raise ImageNotFoundException instead of returning None.
-
-# NOTE:
-# with SB(undetected=True, incognito=True) as driver:
-# with DriverContext(uc=True) as driver: 
-# both these context managers seem not fully compatible with Fedora, 
-# causing error: "X11 display failed! Will use regular xvfb!"
-
-
-# NOTE: on Fedora, need the followings libs for pyautogui
-# sudo dnf install scrot
-# sudo dnf install python3-tkinter
-# sudo dnf install python3-devel
-
 
 from time import sleep
 from contextlib import contextmanager
